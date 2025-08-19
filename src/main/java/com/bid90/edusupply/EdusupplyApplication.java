18f43ab75cb0ec9dbf0f9bd7d6b665bf62efa3d7
Ioan Daniel <ioand339@gmail.com>
2025-08-19 13:19:21 +0300

    first commit

---
+++ src/main/java/com/bid90/edusupply/EdusupplyApplication.java
@@ -0,0 +1,13 @@
+package com.bid90.edusupply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdusupplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdusupplyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bid90/edusupply/EdusupplyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bid90.edusupply;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdusupplyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
